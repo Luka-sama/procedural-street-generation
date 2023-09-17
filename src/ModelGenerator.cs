ea9f72d471d21ab8eab639d43e469d163dc3c1c9
Luka-sama <luka-sama@pm.me>
2023-09-17 21:48:09 +0200

    Revert failed ideas

--- src/ModelGenerator.cs
+++ src/ModelGenerator.cs
@@ -8,7 +8,7 @@ public enum DebugType
 	HighlightTriangles
 }
 
-public partial class ModelGenerator : CsgCombiner3D
+public partial class ModelGenerator : MeshInstance3D
 {
 	public float ModelScale { get; } = 0.3f;
 	private RandomNumberGenerator _rng = new RandomNumberGenerator();
@@ -27,62 +27,20 @@ public partial class ModelGenerator : CsgCombiner3D
 		var cityScheme = (GetNode("../CityScheme") as CityScheme);
 		//GenerateModel(cityScheme.GetMainRoads(), 30, 150);
 		var lerpSteps = new List<float> { 0f, 0.05f, 0.1f, 0.15f, 0.2f, 0.8f, 0.85f, 0.9f, 0.95f, 1f };
-		foreach (var child in GetChildren())
-		{
-			child.QueueFree();
-		}
 		GenerateModel(cityScheme.GetMajorRoads(), 10, lerpSteps, debugType);
 		//GenerateModel(cityScheme.GetMinorRoads(), 5, 50);
-	}
-
-	private CsgMesh3D GetCsgMesh(ArrayMesh mesh, Material material)
-	{
-		var csgMesh = new CsgMesh3D();
-		csgMesh.Mesh = mesh;
-		csgMesh.MaterialOverride = material;
-		return csgMesh;
-	}
-
-	private CsgCombiner3D GetCsgGroup(GodotObject mesh1, GodotObject mesh2, Material material)
-	{
-		var group = new CsgCombiner3D();
-
-		if (mesh1 is ArrayMesh)
-		{
-			group.AddChild(GetCsgMesh(mesh1 as ArrayMesh, material));
-		} else
-		{
-			group.AddChild(mesh1 as Node);
-		}
-
-		if (mesh2 is ArrayMesh)
-		{
-			group.AddChild(GetCsgMesh(mesh2 as ArrayMesh, material));
-		} else
-		{
-			group.AddChild(mesh2 as Node);
-		}
-
-		return group;
 	}
 
 	private void GenerateModel(List<List<Vector2>> roads, float roadWidth, List<float> lerpSteps, DebugType debugType)
 	{
 		_rng.State = _savedState;
-		List<ArrayMesh> meshes = new();
 		
+		var st = new SurfaceTool();
+		st.Begin(Mesh.PrimitiveType.Triangles);
+		st.SetUV(new Vector2(0, 0));
+
 		foreach (var road in roads)
 		{
-			GD.Print(Time.GetTicksMsec());
-			var st = new SurfaceTool();
-			st.Begin(Mesh.PrimitiveType.Triangles);
-			st.SetUV(new Vector2(0, 0));
-			/*for (var i = 0; i < road.Count; i++)
-			{
-				GD.Print(road[i].X + " " + road[i].Y);
-			}
-			GD.Print();*/
-
 			roadWidth = _rng.RandiRange(10, 30);
 			var firstPointFrom = road[0];
 			var firstPointTo = road[1];
@@ -133,42 +91,17 @@ public partial class ModelGenerator : CsgCombiner3D
 					lastTopRight = topRight;
 				}
 			}
-			
-			st.Index();
-			st.GenerateNormals();
-			st.GenerateTangents();
-			var mesh = st.Commit();
-			meshes.Add(mesh);
 		}
-		
+
+		st.Index();
+		st.GenerateNormals();
+		st.GenerateTangents();
+		var mesh = st.Commit();
+		Scale = ModelScale * Vector3.One;
+		Mesh = mesh;
+
 		var material = new StandardMaterial3D();
 		material.VertexColorUseAsAlbedo = true;
-
-		CsgCombiner3D last = GetCsgGroup(
-			GetCsgMesh(meshes[0], material),
-			GetCsgMesh(meshes[1], material),
-			material);
-		var meshNum = 0;
-		foreach (var mesh in meshes)
-		{
-			if (meshNum > 1)
-			{
-				last = GetCsgGroup(last, mesh, material);
-			}
-			meshNum++;
-		}
-		foreach (var child in last.GetChildren())
-		{
-			last.RemoveChild(child);
-			AddChild(child);
-		}
-		GD.Print(Time.GetTicksMsec());
-		
-		Scale = ModelScale * Vector3.One;
-		/*var shader = new ShaderMaterial();
-		shader.Shader = GD.Load<Shader>("res://src/height_map.gdshader");
-		shader.SetShaderParameter("source", GD.Load<Texture2D>("res://height_map.png"));
-		shader.SetShaderParameter("height_range", 10);
-		MaterialOverride = shader;*/
+		MaterialOverride = material;
 	}
 }
